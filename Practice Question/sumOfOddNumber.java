5e8ce55e6da697cc372fdb5ef61e41548cdfd66e
https://github.com/RohitRathi-sigma/LEARNING-DSA.git <rohitrathi9588@gmail.com>
2024-10-30 17:18:45 +0530

    Java

---
+++ Practice Question/sumOfOddNumber.java
@@ -0,0 +1,12 @@
+public class SumOfOddNumber {
+    public static void main(String[] args) {
+        int arr[]={1,32,3,4,5,6,7,8};
+        int sum=0;
+        for(int i=0;i<sizeofarr[];i++){
+            if(num%2!=0){
+                sum+=num;
+            }
+        }
+        System.out.println("sum of odd number: "+sum);
+    }
+}
